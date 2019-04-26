4a83aab2855745d24cfa0ff49f64d8d04f756aa7
Rafael Julio <hello.rjulio@gmail.com>
2019-04-26 13:41:14 -0300

    style: Format code using TSLint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { Command, flags } from '@oclif/command'
-import { createLogger, format, transports } from 'winston'
-import { DownloaderHelper } from 'node-downloader-helper'
-import * as tempy from 'tempy'
-import * as puppeteer from 'puppeteer'
+import {Command, flags} from '@oclif/command'
 import * as cheerio from 'cheerio'
+import {DownloaderHelper} from 'node-downloader-helper'
+import * as puppeteer from 'puppeteer'
+import * as tempy from 'tempy'
+import {createLogger, format, transports} from 'winston'
 
 const Ora = require('ora')
-const { combine, label, timestamp, printf } = format
+const {combine, label, timestamp, printf} = format
 
-const logFormat = printf(({ level, message, label, timestamp }) => {
+const logFormat = printf(({level, message, label, timestamp}) => {
   return `${timestamp} [${label}] ${level}: ${message}`
 })
 
@@ -18,11 +18,11 @@ const baseUrl = 'https://twist.moe'
  * Function created by hgouveia, modified by rafifos.
  * The original source code can be found at: https://github.com/hgouveia/node-downloader-helper/blob/0b3f2e42d72bdc24ab0889a8954161d239659fb6/example/helpers.js#L4
  */
-function byteHelper(value:any) {
+function byteHelper(value: any) {
   // https://gist.github.com/thomseddon/3511330
   let units = ['b', 'kB', 'MB', 'GB', 'TB']
-  let number = Math.floor(Math.log(value) / Math.log(1024));
-  return (value / Math.pow(1024, Math.floor(number))).toFixed(1) + ' ' + units[number];
+  let number = Math.floor(Math.log(value) / Math.log(1024))
+  return (value / Math.pow(1024, Math.floor(number))).toFixed(1) + ' ' + units[number]
 }
 
 class Atdownloader extends Command {
@@ -46,54 +46,58 @@ class Atdownloader extends Command {
     }
   ]
 
-  async getDownloadLink(url: string) {
-    // TODO: Treat errors beautifully
+  async getDownloadLink(url: string, verbose: boolean) {
+    const logger = createLogger({
+      transports: [
+        new transports.File({filename: tempy.file({extension: 'log'})}),
+        new transports.Console()
+      ],
+      format: combine(
+        label({label: 'atdownloader'}),
+        timestamp(),
+        logFormat
+      ),
+      silent: !verbose || false
+    })
     const browser = await puppeteer.launch()
-    const page = await browser.newPage();
+    const page = await browser.newPage()
 
     await page.goto(url)
 
     let content = await page.content()
-    var $ = await cheerio.load(content)
+    let $ = await cheerio.load(content)
 
-    browser.close()
+    browser.close().catch(err => {
+      logger.error(err)
+    })
 
     return baseUrl + $('video').attr('src')
   }
 
   async run() {
-    const { args, flags } = this.parse(Atdownloader)
+    const {args, flags} = this.parse(Atdownloader)
 
     const logger = createLogger({
-      levels: {
-        error: 0,
-        warn: 1,
-        info: 2,
-        verbose: 3,
-        debug: 4,
-        silly: 5
-      },
-      level: 'verbose',
       transports: [
-        new transports.File({ filename: tempy.file({ extension: 'log' }) }),
+        new transports.File({filename: tempy.file({extension: 'log'})}),
         new transports.Console()
       ],
       format: combine(
-        label({ label: 'atdownloader' }),
+        label({label: 'atdownloader'}),
         timestamp(),
         logFormat
       ),
       silent: !flags.verbose || false
     })
 
-    var dest:string = flags.dest || process.env.HOME + '/Downloads'
+    let dest: string = flags.dest || process.env.HOME + '/Downloads'
 
     if (!args.url.match(baseUrl)) {
       logger.info('Invalid url. Exiting...')
       this.exit(1)
     }
 
-    const downloadLink = await this.getDownloadLink(args.url)
+    const downloadLink = await this.getDownloadLink(args.url, flags.verbose)
     logger.info(`Link found: ${downloadLink}`)
 
     const spinner = new Ora({
@@ -104,12 +108,14 @@ class Atdownloader extends Command {
     logger.info('Download started.')
     spinner.start()
     const dl = new DownloaderHelper(downloadLink, dest)
-    dl.on('progress', (stats) => {
+    dl.on('progress', stats => {
       spinner.text = 'Downloading: ' + byteHelper(stats.downloaded) + '/' + byteHelper(stats.total) + ' | ' + byteHelper(stats.speed) + '/s' + ' | ' + stats.progress.toFixed(1) + '%'
     })
-    dl.on('error', (err) => logger.error(`Download failed: ${err}`))
+    dl.on('error', err => logger.error(`Download failed: ${err}`))
     dl.on('end', () => logger.info('Download completed.'))
-    dl.start()
+    dl.start().catch(err => {
+      logger.error(err)
+    })
   }
 }
 
