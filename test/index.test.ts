0c975142c84b52e3e74219711346aa5a88351b17
Rafael Julio <hello.rjulio@gmail.com>
2019-05-06 16:40:32 -0300

    test: Write initial tests
    These tests do 2 simple things, try to run the application with an invalid URL, and see whether or
    not it downloads an anime correctly.

---
+++ test/index.test.ts
@@ -0,0 +1,27 @@
+import {test} from '@oclif/test'
+
+import cmd = require('../src/index')
+
+describe('atdownloader', () => {
+  test
+    .stdout()
+    .do(() => cmd.run(['https://dogemuchwow.com/wp-content/uploads/2017/11/web-developer-doge.jpg']))
+    .exit(4)
+    .it('runs with invalid url')
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://twist.moe/a/boku-no-pico/1']))
+    .exit(0)
+    .it('downloads an anime')
+
+  /*
+   * This contains an example of a test.
+   * test
+   *   .stdout()
+   *   .do(() => cmd.run(['--name', 'jeff']))
+   *   .it('runs with invalid url', ctx => {
+   *     expect(ctx.stdout).to.contain('hello jeff')
+   *   })
+   */
+})
