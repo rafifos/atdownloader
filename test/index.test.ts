31de58641f292cc08584ef99ecf7db85cea84246
Rafael Julio <hello.rjulio@gmail.com>
2019-05-06 17:37:04 -0300

    test: Drop download test
    Too much issues with it atm.

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,18 +9,6 @@ describe('atdownloader', () => {
     .exit(4)
     .it('runs with invalid url')
 
-  test
-    .stdout()
-    .do(() => cmd.run(['https://twist.moe/a/boku-no-pico/1']))
-    .exit(0)
-    .it('downloads a old anime')
-
-  test
-    .stdout()
-    .do(() => cmd.run(['https://twist.moe/a/mob-psycho-100-ii/1']))
-    .exit(0)
-    .it('downloads a new anime')
-
   /*
    * This contains an example of a test.
    * test
